3e608a173053b3f4064a5a276303d4073b9f7d05
Piyumi Wijesinghe <pswijesinghe001@gmail.com>
2025-06-17 09:24:01 +0530

    Add button for signup js part
    Add a button to select whether you are a buyer or a seller

--- signup/src/Signupform.js
+++ signup/src/Signupform.js
@@ -10,6 +10,26 @@ function SignUpForm() {
         <h1>AgriBid.lk</h1>
         <p className="tagline">The modern marketplace that directly connects farmers and buyers!</p>
        </div>
+       <div className="signup-right">
+        <h2>Get Started</h2>
+        <p>By creating a free account</p>
+
+        <div className="role-toggle">
+          <button
+            className={role === 'Buyer' ? 'active' : ''}
+            onClick={() => setRole('Buyer')}
+          >
+            Buyer
+          </button>
+          <button
+            className={role === 'Seller' ? 'active' : ''}
+            onClick={() => setRole('Seller')}
+          >
+            Seller
+          </button>
+        </div>
+
+      </div>
 
       
     </div>
