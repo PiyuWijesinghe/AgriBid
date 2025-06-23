5fa968dcd1884f88979e46ab0d56b5d50556f80e
Piyumi Wijesinghe <pswijesinghe001@gmail.com>
2025-06-23 12:48:32 +0530

    create place bid page

---
+++ placebid/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Placebid from './Placebid';
+
+function App() {
+  return (
+    <div className="App">
+      <Placebid />
+    </div>
+  );
+}
+
+export default App;
